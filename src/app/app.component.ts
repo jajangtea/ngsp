803fa8a62ed394b6d98a7372afdc27e37cfc747f
jajangtea <za2ng2509@gmail.com>
2019-02-16 15:15:06 +0700

    cssw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  netImage:any = "../themes/img/profile.png";
   title = 'ngsp';
   judul = 'APLIKASI CETAK SURAT PENGANTAR KERJA PRAKTEK DAN SKRIPSI';
 }
