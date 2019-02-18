97ee701cee123202cf831a09709b9a57fc64afcd
jajangtea <za2ng2509@gmail.com>
2019-02-18 14:42:31 +0700

    update

--- src/app/sp_kp/view/view.component.ts
+++ src/app/sp_kp/view/view.component.ts
@@ -40,10 +40,10 @@ export class ViewComponent implements OnInit {
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/jpeg')
+      const contentDataURL = canvas.toDataURL('image/png', 1.0)
       let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
       var position = 0;
-      pdf.addImage(contentDataURL, 'JPEG', 20, 40, 170, 160)
+      pdf.addImage(contentDataURL, 'PNG', 20, 40, 170, 160)
       pdf.save('MYPdf.pdf'); // Generated PDF 
     });
   }
