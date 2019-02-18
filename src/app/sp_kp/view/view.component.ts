dc7896d6f047ec1d478e9aaeb8c5e711a225800e
jajangtea <za2ng2509@gmail.com>
2019-02-18 12:21:04 +0700

    template surat

--- src/app/sp_kp/view/view.component.ts
+++ src/app/sp_kp/view/view.component.ts
@@ -40,10 +40,10 @@ export class ViewComponent implements OnInit {
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png')
+      const contentDataURL = canvas.toDataURL('image/jpeg')
       let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
       var position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.addImage(contentDataURL, 'JPEG', 20, 40, 170, 160)
       pdf.save('MYPdf.pdf'); // Generated PDF 
     });
   }
